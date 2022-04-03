79420a064383d10e153bb6aca29621a9e1d21bb3
anas-awata <anasawata7@gmail.com>
2022-04-03 19:22:43 +0200

    second commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="logo">STORE-TEMPLATE</div>
+    <ul>
+      <li>
+        <router-link to="/"><a>Home</a></router-link>
+      </li>
+      |
+      <li>
+        <router-link to="/about"><a>About</a></router-link>
+      </li>
+      |
+      <li>
+        <router-link to="/help"><a> Help</a></router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+<style lang="scss" scoped>
+.header {
+  background: #333;
+  color: #fff;
+  overflow: hidden;
+  .logo {
+    font-size: 24px;
+    padding: 20px;
+    float: left;
+    width: 300px;
+  }
+  ul {
+    float: left;
+    width: 800px;
+    li {
+      padding: 20px;
+      display: inline-block;
+      a {
+        text-decoration: none;
+        font-size: 18px;
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/help.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="help">
+    <h1>help</h1>
+    <p>sdfghjvbnfghghj</p>
+    <hello-world />
+  </div>
+</template>
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "my-help",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
